eb8c3b87db3280b57475be340c596c38ad44e2bb
LatinaEnjoyer <nioe1000@stud.ek.dk>
2026-09-14 14:08:14 +0200

    step 1 complete

---
+++ src/main/java/ek/osnb/starter/model/Rating.java
@@ -0,0 +1,35 @@
+package ek.osnb.starter.model;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Rating {
+    private Double score;      // e.g., 8.5
+    private Integer voteCount; // e.g., 15000
+
+    // TODO: Add default constructor
+    public Rating(){}
+
+    // TODO: Add constructor with parameters
+    public Rating(Double score, Integer voteCount) {
+        this.score = score;
+        this.voteCount = voteCount;
+    }
+
+    // TODO: Add getters and setters
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+}
